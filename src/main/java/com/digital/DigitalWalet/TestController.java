57e024645ebdaec1ff0a1bc53bc36571078e3063
muluadam <muluadamt@gmail.com>
2021-07-02 00:37:38 -0500

    test

---
+++ src/main/java/com/digital/DigitalWalet/TestController.java
@@ -0,0 +1,20 @@
+package com.digital.DigitalWalet;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test2")
+    public String testTwo(){
+        return "Test Two after push  three /from to from github ";
+    }
+    @GetMapping("/hello")
+    public String testTwos(){
+        return "Hello from Github";
+    }
+    @GetMapping("/hello2")
+    public String testTwos2(){
+        return "Hello method two is changed sdf ";
+    }
+}
