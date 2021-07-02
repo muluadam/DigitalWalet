b059866be9abc07a9edb9900e14a88eff87eab96
muluadam <muluadamt@gmail.com>
2021-07-02 00:34:33 -0500

    First Commit

---
+++ src/main/java/com/digital/DigitalWalet/DigitalWaletApplication.java
@@ -0,0 +1,21 @@
+package com.digital.DigitalWalet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@RestController
+public class DigitalWaletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalWaletApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String get(){
+		return "Hello World";
+	}
+}
---
+++ src/test/java/com/digital/DigitalWalet/DigitalWaletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.DigitalWalet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWaletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
