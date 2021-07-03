cc98f2c29f06ccef3290523df22914a23bfea215
muluadam <muluadamt@gmail.com>
2021-07-03 00:00:19 -0500

    test26

--- src/main/java/com/digital/DigitalWalet/DigitalWaletApplication.java
+++ src/main/java/com/digital/DigitalWalet/DigitalWaletApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 //@RestController
 @Configuration
 
-@SpringBootApplication
+@SpringBootApplication  //added for MySQL
+
 @EnableScheduling
 public class DigitalWaletApplication {
 
