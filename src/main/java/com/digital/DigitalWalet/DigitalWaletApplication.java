6d011e65f16b9f137b1d5fea860c94c8cea7b0d9
muluadam <muluadamt@gmail.com>
2021-07-03 12:09:01 -0500

    test26

--- src/main/java/com/digital/DigitalWalet/DigitalWaletApplication.java
+++ src/main/java/com/digital/DigitalWalet/DigitalWaletApplication.java
@@ -1,6 +1,5 @@
 package com.digital.DigitalWalet;
 
-import com.digital.DigitalWalet.repository.CustomerRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
